68729e61d761cb49de4327faa9b05b9f85c6457b
JKOOUP <evdgenij@mail.ru>
2021-02-03 09:16:31 +0000

    Added downloading minus checker model

--- setup.sh
+++ setup.sh
@@ -16,10 +16,14 @@ wget http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
 tar -xvf vgg_16_2016_08_28.tar.gz
 rm -r /content/DLSProject/data/vgg_16_2016_08_28.tar.gz
 
-echo "Downloading recgnition model"
+echo "Downloading recognition model"
 cd ..
 gdown 'https://drive.google.com/uc?id=1a-0qujaO461ViSOl4GT6EgwH5uqc6xgO'
 mv ./BestMoran.pth ./checkpoints_mlt/
+
+echo "Downloading minuses checker model"
+gdown 'https://drive.google.com/uc?id=1jUOTdKgOnUX87s7F_BYogBi20QBxLMf3'
+mv ./minus_checker.pth ./checkpoints_mlt/
 
 cd ./data/
 mkdir cropped
